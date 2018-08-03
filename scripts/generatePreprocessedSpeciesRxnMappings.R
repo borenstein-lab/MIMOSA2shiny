1dbd5e7e8d405c0c8729c739bfae33a89d1ccf95
Cecilia Noecker <cecilia.noecker@gmail.com>
2018-08-03 15:44:15 -0700

    partial network preprocessing

--- scripts/generatePreprocessedSpeciesRxnMappings.R
+++ scripts/generatePreprocessedSpeciesRxnMappings.R
@@ -26,4 +26,14 @@ if(database == "PICRUSt"){
     spec_mod = generate_genomic_network(contribution_table[OTU==x, unique(Gene)], keggSource = "KeggTemplate", degree_filter = 30, rxn_table = network_template, return_mats = F)
     spec_mod[,OTU:=x]
   }))
+} else { #AGORA
+  otu_list = list.files(path = "data/AGORA/", pattern = ".mat")
+  otu_list = gsub(".mat$", "", otu_list)
+  all_mods = load_agora_models(otu_list)
+  all_mods = get_S_mats(all_mods, otu_list, edge_list = T)
+  #Should we split up? Depends how big it is.
+  for(spec in otu_list){
+    write.table(all_mods[OTU==spec], file = paste0("data/AGORA/", spec, "_rxns.txt"), quote=F, row.names=F, sep = "\t")
+  }
+
 }
