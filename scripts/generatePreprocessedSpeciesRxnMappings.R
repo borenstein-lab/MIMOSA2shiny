56afab054d1f7e4cf9a08381d22aea9aefef56d1
Cecilia Noecker <cecilia.noecker@gmail.com>
2018-09-11 14:40:02 -0700

    include 16S normalization in species-network generation

--- scripts/generatePreprocessedSpeciesRxnMappings.R
+++ scripts/generatePreprocessedSpeciesRxnMappings.R
@@ -10,11 +10,12 @@ if(database == "PICRUSt"){
   picrust_ko_table_directory = "data/picrustGenomeData/indivGenomes/"
   picrust_ko_table_suffix = "_genomic_content.tab"
   all_otus = gsub(picrust_ko_table_suffix, "", list.files(picrust_ko_table_directory))
-  # picrust_norm_file = "data/picrustGenomeData/16S_13_5_precalculated.tab.gz"
-  # Get normalization data
-  # picrust_normalization_table = fread(paste("gunzip -c ", picrust_norm_file, sep=""), header=T)
-  # colnames(picrust_normalization_table) = c("OTU", "norm_factor")
-  # picrust_normalization_table[,OTU:= as.character(OTU)]
+
+  picrust_norm_file = "data/picrustGenomeData/16S_13_5_precalculated.tab"
+  #Get normalization data
+  picrust_normalization_table = fread(picrust_norm_file, header = T)#fread(paste("gunzip -c ", picrust_norm_file, sep=""), header=T)
+  colnames(picrust_normalization_table) = c("OTU", "norm_factor")
+  picrust_normalization_table[,OTU:= as.character(OTU)]
   
   #Merge with reaction info
   kegg_paths = c("data/KEGGfiles/reaction_mapformula.lst", "data/KEGGfiles/reaction_ko.list", "data/KEGGfiles/reaction")
@@ -26,9 +27,12 @@ if(database == "PICRUSt"){
   }
   for(x in all_otus){
       genomic_content = get_genomic_content_from_picrust_table(x, picrust_ko_table_directory, picrust_ko_table_suffix)
-      spec_mod = generate_genomic_network(genomic_content[,unique(Gene)], keggSource = "KeggTemplate", degree_filter = 30, rxn_table = network_template, return_mats = F)
+      spec_mod = generate_genomic_network(genomic_content[,unique(Gene)], keggSource = "KeggTemplate", degree_filter = 0, rxn_table = network_template, return_mats = F) #Not going to filter anymore
       #Also get copy number info
       spec_mod = merge(spec_mod, genomic_content, by.x = "KO", by.y = "Gene", all.x=T)
+      spec_mod = merge(spec_mod, picrust_normalization_table, by = "OTU", all.x=T, all.y=F)
+      spec_mod[,normalized_copy_number:=copy_number/norm_factor]
+      spec_mod = spec_mod[,list(OTU, KO, Reac, Prod, stoichReac, stoichProd, normalized_copy_number)]
       write.table(spec_mod, file = paste0("data/picrustGenomeData/indivModels/", x, "_rxns.txt"), quote=F, row.names=F, sep = "\t")
   }
 } else { #AGORA
@@ -36,10 +40,14 @@ if(database == "PICRUSt"){
   otu_list = gsub(".mat$", "", otu_list)
   all_mods = load_agora_models(otu_list)
   all_mods = get_S_mats(all_mods, otu_list, edge_list = T)
-  #Should we split up? Depends how big it is.
+  ##Read back in and add copy number info????
+  genome_info = fread("data/blastDB/agora_NCBItax_processed_nodups.txt")
+  all_mods[,copy_number:=1]
+  all_mods = merge(all_mods, genome_info[,list(AGORA_ID, CopyNum)], all.x = T, by.x = "Species", by.y = "AGORA_ID")
+  all_mods[,normalized_copy_number:=ifelse(is.na(CopyNum), 1, copy_number/CopyNum)]
+  all_mods = all_mods[,list(Species, KO, Reac, Prod, stoichReac, stoichProd, normalized_copy_number)]
   for(spec in otu_list){
     write.table(all_mods[Species==spec], file = paste0("data/AGORA/", spec, "_rxns.txt"), quote=F, row.names=F, sep = "\t")
   }
-  ##Read back in and add copy number info????
 
 }
