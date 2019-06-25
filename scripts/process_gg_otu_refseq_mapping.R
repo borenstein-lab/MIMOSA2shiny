d1a48867d94d5eac3888110cb69465d9e9b90be4
CECILIA A. NOECKER <cnoecker@elbo-spice.gs.washington.edu>
2019-06-24 22:20:17 -0700

    otu-refseq mapping processing

--- scripts/process_gg_otu_refseq_mapping.R
+++ scripts/process_gg_otu_refseq_mapping.R
@@ -15,8 +15,8 @@ for(j in 1:length(vsearch_results_files)){
 	foo[,assemblyID:=gsub("_lcl.*$", "", V2)]
 	foo = unique(foo)
 	map_all = merge(foo, genome_info, by.x = "assemblyID", by.y = "assembly_accession", all.x = T)
-	map_all = map_all[,list(assemblyID, V1, V2, V3, V4, CopyNum16S)]
-	setnames(map_all, c("Ref_ID", "OTU", "fullGeneID", "matchPerc", "matchLen", "CopyNum"))
+	map_all = map_all[,list(ModelID, assemblyID, V1, V2, V3, V4, CopyNum16S)]
+	setnames(map_all, c("ModelID", "Ref_ID", "OTU", "fullGeneID", "matchPerc", "matchLen", "CopyNum"))
 	write.table(map_all, file = out_files[j], quote=F, row.names=F, sep = "\t")
 }
 
